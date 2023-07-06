f7ec1a158f771d26a37d29f3703c358e56b12d3a
liumengniu <1*AveqrjUE5LlCHA0Gtj>
2023-07-06 16:49:50 +0800

    init pro

--- src/pages/auto-paper/index.js
+++ src/pages/auto-paper/index.js
@@ -3,11 +3,13 @@
  * @author Kevin
  * @date 2023/7/5
  */
+import _ from 'lodash';
 import "./index.scss"
-import {Button, Form, Input, InputNumber, Steps} from "antd";
+import {Button, Form, Input, InputNumber, Radio, Steps} from "antd";
 import {MinusOutlined, PlusOutlined} from "@ant-design/icons"
 import ThButton from "@comp/button";
-import {generate_title} from "@services/generate";
+import {generate_outline, generate_title} from "@services/generate";
+import {useState} from "react";
 
 const {TextArea} = Input;
 
@@ -36,6 +38,20 @@ const items = [
 
 function AutoPaper() {
 	const [form] = Form.useForm()
+	const [form2] = Form.useForm()
+	const [form3] = Form.useForm()
+	const [current, setCurrent] = useState(0)
+	const [caption, setCaption] = useState("") // 标题
+	const [outline, setOutline] = useState("") // 大纲
+	
+	
+	/**
+	 * 获取标题列表
+	 * @returns {*[]|T[]}
+	 */
+	const getRadioList = () => {
+		return _.isEmpty(caption) ? [] : (_.split(caption,"\n") || []).filter((o,idx) => !_.isEmpty(o))
+	}
 	/**
 	 * 第一步
 	 */
@@ -43,9 +59,29 @@ function AutoPaper() {
 		try {
 			let values = await form.validateFields()
 			const cb = str => {
+				setCurrent(current + 1)
 				console.log(str, 'str=======================strstr')
+				setCaption(str)
 			}
 			await generate_title({...values, cb})
+		} catch (e) {
+		
+		}
+	}
+	/**
+	 * 第二步
+	 * @returns {Promise<void>}
+	 */
+	const secondStep = async () =>{
+		try {
+			let values = await form2.validateFields()
+			const cb = str => {
+				setCurrent(current + 1)
+				console.log(str, 'str=======================strstr')
+				setOutline(str)
+				form3.setFieldsValue({outline: str})
+			}
+			await generate_outline({...values, cb})
 		} catch (e) {
 		
 		}
@@ -54,9 +90,9 @@ function AutoPaper() {
 	return (
 		<div className="auto-paper">
 			<div className="auto-paper-box">
-				<Steps current={0} items={items}/>
+				<Steps current={current} items={items}/>
 				{/*第一步*/}
-				<div className="form step-1">
+				<div className="form step-1" style={{display: current === 0 ? "block" : "none"}}>
 					<Form form={form} layout="vertical">
 						<Form.Item label="报告类型" name="title" rules={[{required: true, message: '请输入报告类型!'}]}>
 							<TextArea style={{height: 80, resize: "none", borderRadius: 4}} maxLength={10} showCount
@@ -78,8 +114,46 @@ function AutoPaper() {
 					</Form>
 				</div>
 				{/*第二步*/}
-				<div className="form step-2">
-				
+				<div className="form step-2" style={{display: current === 1 ? "block" : "none"}}>
+					<Form form={form2} layout="vertical">
+						<Form.Item label="选择标题" name="title" rules={[{required: true, message: '请选择标题!'}]}>
+							<Radio.Group>
+								{
+									_.map(getRadioList(), (o, idx) => <Radio value={o} key={idx}>{o}</Radio>)
+								}
+							</Radio.Group>
+						</Form.Item>
+						<Form.Item label="添加大纲生成要求" name="requirement" rules={[{required: true, message: '请添加大纲生成要求!'}]}>
+							<TextArea rows={8} style={{ height: 200, resize: "none", borderRadius: 4 }} showCount placeholder="您可以继续输入对于文章大纲的要求，如层次分明，逻辑严谨，章节序号依次按照“一、”“（一）、”“1、”“（1）、”进行标注"/>
+						</Form.Item>
+						<Form.Item>
+							<ThButton title="下一步" type="primary" block onClick={secondStep}/>
+						</Form.Item>
+					</Form>
+				</div>
+				{/*第三步*/}
+				<div className="form step-2" style={{display: current === 2 ? "block" : "none"}}>
+					<Form form={form3} layout="vertical">
+						<Form.Item name="outline" initialValue={outline} label="大纲"  rules={[{required: true, message: '请添加大纲!'}]}>
+							<TextArea
+								autoSize={{ minRows: 3, maxRows: 30 }}
+								value={outline}
+								showCount
+								style={{ minHeight: 120, paddingBottom: 20, resize: "none", borderRadius: 4 }}
+								onChange={e => setOutline(e.target.value)}
+								placeholder="输入大纲信息"
+							/>
+						</Form.Item>
+						<Form.Item name="requirement" label="添加全文生成要求"  rules={[{required: true, message: '请添加文章全文的生成要求!'}]}>
+							<TextArea
+								showCount
+								maxLength={500}
+								style={{ height: 120, resize: "none", borderRadius: 4 }}
+								placeholder="您可以继续输入对于文章全文的生成要求，如结构清晰，内容详细，每个小点至少编写300字"
+							/>
+						</Form.Item>
+					</Form>
+					
 				</div>
 			</div>
 		
--- src/services/generate.js
+++ src/services/generate.js
@@ -14,3 +14,12 @@ import request from "@utils/request"
 export function generate_title(data) {
 	return request.post("/generate_title", { data },"sse")
 }
+
+/**
+ * 获取标题
+ * @param data
+ * @returns {Promise<*>}
+ */
+export function generate_outline(data) {
+	return request.post("/generate_outline", { data },"sse")
+}
